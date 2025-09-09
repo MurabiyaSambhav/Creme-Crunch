a2b3a24d5917d24d95ffdacf51360c872aad0dbc
kinjal <rathodkinjalba4462@gmail.com>
2025-09-09 16:00:54 +0530

    Update views.py

--- bakery/views.py
+++ bakery/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Djangoooooo0vjvvhvv0g00000 ")
+    return HttpResponse("Hello, Djangoooooo0vjvvhvv0g00000000 ")
