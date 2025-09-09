035689c3028625c89be9ca48f26fd32b07767613
kinjal <rathodkinjalba4462@gmail.com>
2025-09-09 16:24:50 +0530

    Update views.py

--- bakery/views.py
+++ bakery/views.py
@@ -1,8 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-<<<<<<< Updated upstream
-    return HttpResponse("Hello, Djangoooooo0vjvvhvv0g00000000ghjgh,m,bm,nbjghj ")
-=======
     return HttpResponse("")
->>>>>>> Stashed changes
