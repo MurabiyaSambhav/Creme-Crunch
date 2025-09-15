f18f28a20210f57b83a0f05378bff4ff5f4f3c09
kinjal <rathodkinjalba4462@gmail.com>
2025-09-15 10:40:19 +0530

    commit

--- bakery/views.py
+++ bakery/views.py
@@ -6,6 +6,11 @@ from django.conf import settings
 
 def base(request):
     return render(request,'base.html')
+from django.conf import settings
+
+
+def base(request):
+    return render(request,'base.html')
 
 def home(request):
     return render(request, "home.html")
