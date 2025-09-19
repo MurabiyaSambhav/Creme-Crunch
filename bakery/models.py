3afb0e7f014fdf8e3a0d1ebfecb911c706367c74
MurabiyaSambhav <sambhavmurabiya@gmail.com>
2025-09-19 16:32:51 +0530

    simple change

--- bakery/models.py
+++ bakery/models.py
@@ -29,7 +29,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     subcategories = models.ManyToManyField(Category, blank=True, related_name="sub_products")  # multiple subcategories
-    image = models.ImageField(upload_to="products/")
     description = models.TextField()
     def __str__(self):
         return f"{self.product.name} - Image"
